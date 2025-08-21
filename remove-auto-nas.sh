7ee9e07f7c9032e26bd144be6fa448bd99f7bcbb
Omar Kada <kadaomar@hotmail.com>
2025-08-21 15:41:34 +0200

    add syncthing - remove labels in compose

--- remove-auto-nas.sh
+++ remove-auto-nas.sh
@@ -3,6 +3,16 @@ set -euo pipefail
 
 echo "🚫 remove all containers managed by autonas"
 
-docker ps -q --filter "label=com.autonas.managed=true" | xargs -r docker rm -f
+ENV_NAME="AUTONAS_MANAGED"
+ENV_VALUE="true"
+
+docker ps -aq | while read id; do
+    echo "Checking container $id for environment variable $ENV_NAME=$ENV_VALUE"
+  # Check if container has the desired environment variable
+  if docker inspect -f '{{range .Config.Env}}{{println .}}{{end}}' "$id" | grep -q "^$ENV_NAME=$ENV_VALUE$"; then
+    echo "Removing container $id"
+    docker rm -f "$id"
+  fi
+done
 
 echo "✅ all containers removed"
