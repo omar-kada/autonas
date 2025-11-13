ad0dd4165b3d34b7eb04b3b9904b310a0525f0f9
Omar Kada <kadaomar@hotmail.com>
2025-11-13 19:24:15 +0100

    add more coverage to tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/elliotchance/orderedmap/v3"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConfigPerService_BuildsCorrectArray(t *testing.T) {
@@ -29,4 +30,28 @@ func TestConfigPerService_BuildsCorrectArray(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("ConfigPerService mismatch\nwant=%#v\ngot =%#v", want, got)
 	}
+}
+
+func TestGetEnabledServices_FiltersCorrectly(t *testing.T) {
+	cfg := Config{
+		Extra: map[string]any{
+			"GLOBAL": "g",
+		},
+		Services: map[string]ServiceConfig{
+			"svc": {
+				Extra: map[string]any{
+					"SVC_EXTRA": "s",
+				},
+			},
+			"svc2": {
+				Disabled: true,
+				Extra: map[string]any{
+					"SVC_EXTRA": "s",
+				},
+			},
+		},
+	}
+
+	want := []string{"svc"}
+	assert.EqualValues(t, want, cfg.GetEnabledServices())
 }
