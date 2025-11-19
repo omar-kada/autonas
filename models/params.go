c727b8ab0dc940d5e09811868e9b18cdad52ccea
Omar Kada <kadaomar@hotmail.com>
2025-11-19 17:28:09 +0100

    fix linting

--- models/params.go
+++ models/params.go
@@ -10,12 +10,12 @@ type DeploymentParams struct {
 	AddWritePerm bool
 }
 
+// GetAddWritePerm returns the permissions to add based on the AddWritePerm boolean
 func (p DeploymentParams) GetAddWritePerm() os.FileMode {
 	if p.AddWritePerm {
 		return os.FileMode(0666)
-	} else {
-		return os.FileMode(0000)
 	}
+	return os.FileMode(0000)
 }
 
 // ServerParams groups parameters related to the API server
